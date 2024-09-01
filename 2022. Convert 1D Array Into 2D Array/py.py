73133b7a3a26c9a9043da027b9846c5a4874c63c
Kadouci Abdelhak <abdelhak.kadouci@ensia.edu.dz>
2024-09-02 00:49:24 +0100

    added a new python solution

---
+++ 2022. Convert 1D Array Into 2D Array/py.py
@@ -0,0 +1,18 @@
+class Solution:
+    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        #m: number of rows
+        #n: number of cols
+        length= len(original)
+        if (length != m*n):
+            return []
+            print(h)
+        
+        res= []
+        
+        for i in range(m):
+            arr= []
+            for j in range(i*n,n*(1+i)):
+                arr.append(original[j])
+            res.append(arr)
+        
+        return res
